7415fcee9ed7e672d68615db819bc0721d046e03
Ruby <ruby@192.168.1.3>
2024-02-20 00:26:13 +0800

    text2image module

---
+++ text2image/__init__.py
@@ -0,0 +1,2 @@
+from .zhipu_ai_image import *
+from .sd_gen_image import *
---
+++ text2image/sd_gen_image.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+from diffusers import DiffusionPipeline
+import os
+import torch
+import logging
+
+class SDGenImage:
+    def __init__(self, model_path, lora_path = None, lora_scale = 1.0):
+        # 这里先只考虑有1个lora scale:
+        os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
+
+        # model_path="/root/data/model/stable-diffusion-v1-5/"
+        self.pipe = DiffusionPipeline.from_pretrained(model_path, torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
+        self.pipe.to("cuda")
+        # lora_path = "/root/data/TheGodOfCookery/lora/meishi.safetensors"
+        if lora_path is not None:
+            self.pipe.load_lora_weights(lora_path)
+            self.pipe.fuse_lora(lora_scale = lora_scale)
+        # if using torch < 2.0
+        # pipe.enable_xformers_memory_efficient_attention()
+
+        # prompt = "An astronaut riding a green horse"
+
+        # images = pipe(prompt=prompt).images[0]
+
+    def create_img(self, prompt):
+        try:
+            logging.info("[Stable Diffusion] image_query={}".format(prompt))
+            img = self.pipe('meishi,' + prompt).images[0]
+            return True, img
+        except Exception as e:
+            logging.exception(e)
+            return False, "画图出现问题，请休息一下再问我吧"
