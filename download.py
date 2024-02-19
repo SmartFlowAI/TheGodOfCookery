308136f08e540d4db8c02ffb6cf3e975c42b4450
Your Name <you@example.com>
2024-02-19 20:03:47 +0800

    replace whisper with paraformer

--- download.py
+++ download.py
@@ -1,15 +1,15 @@
 
 import os
+from funasr import AutoModel
 from modelscope import snapshot_download
-import whisper
 os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
 model_root_dir = os.environ.get('HOME') + '/models/'
 
 # download  shishen model
-_ = snapshot_download('zhanghuiATchina/zhangxiaobai_shishen2_full',
-                              cache_dir=model_root_dir + 'shishen2')
+# _ = snapshot_download('zhanghuiATchina/zhangxiaobai_shishen2_full',
+#                               cache_dir=model_root_dir + 'shishen2')
 
-# download whisper models
-scales = ["tiny", "base", "small", "medium", "large"]
-for scale in scales:
-    whisper.load_model(scale,download_root=model_root_dir+'whisper')
+# download paraformer models
+model = AutoModel(
+    model='damo/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-pytorch', 
+    model_revision="v2.0.4")
