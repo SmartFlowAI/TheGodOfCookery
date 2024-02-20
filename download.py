57ae3a47b20d423ec45f41752cd82b98496666ed
zhanghui_china <63148804+zhanghui-china@users.noreply.github.com>
2024-02-21 04:33:14 +0800

    Update download.py

--- download.py
+++ download.py
@@ -1,13 +1,14 @@
-
 import os
 from modelscope import snapshot_download
 import whisper
-# download  shishen model
-# model_dir = snapshot_download('zhanghuiATchina/zhangxiaobai_shishen2_full')
-os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
 
-# # download m3e model
-command_str = 'huggingface-cli download --resume-download moka-ai/m3e-base --local-dir-use-symlinks False --local-dir '+ os.environ.get('HOME') + '/models/m3e-base'
+# download shishen model
+# model_dir = snapshot_download('zhanghuiATchina/zhangxiaobai_shishen2_full')
+
+# download m3e model
+os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
+#command_str = 'huggingface-cli download --resume-download moka-ai/m3e-base --local-dir-use-symlinks False --local-dir '+ os.environ.get('HOME') + '/models/m3e-base'
+command_str = 'huggingface-cli download --resume-download moka-ai/m3e-base --local-dir-use-symlinks False --local-dir /home/xlab-app-center/models/m3e-base'
 os.system(command_str)
 
 # download whisper models
