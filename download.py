32c00a31d7e95269c8d1b3861edac7a8c617ab26
zhanghui_china <63148804+zhanghui-china@users.noreply.github.com>
2024-02-23 13:03:33 +0800

    Update download.py

--- download.py
+++ download.py
@@ -28,11 +28,11 @@ if not os.path.exists(os.environ.get('HOME') + '/models/m3e-base'):
 # download RAG model
 print("Download bce embudding base model")
 if not os.path.exists(os.environ.get('HOME') + '/models/bce-embedding-base_v1'):
-    command_str = 'huggingface-cli download --token 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 --resume-download maidalun1020/bce-embedding-base_v1 --local-dir-use-symlinks False --local-dir '+ os.environ.get('HOME') + '/models/bce-embedding-base_v1'
+    command_str = 'huggingface-cli download --token hf_xxx --resume-download maidalun1020/bce-embedding-base_v1 --local-dir-use-symlinks False --local-dir '+ os.environ.get('HOME') + '/models/bce-embedding-base_v1'
     os.system(command_str)
 
 print("Download bce reranker base model")
 if not os.path.exists(os.environ.get('HOME') + '/models/bce-reranker-base_v1'):
-    command_str = 'huggingface-cli download --token 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 --resume-download maidalun1020/bce-reranker-base_v1 --local-dir-use-symlinks False --local-dir '+ os.environ.get('HOME') + '/models/bce-reranker-base_v1'
+    command_str = 'huggingface-cli download --token hf_xxx --resume-download maidalun1020/bce-reranker-base_v1 --local-dir-use-symlinks False --local-dir '+ os.environ.get('HOME') + '/models/bce-reranker-base_v1'
     os.system(command_str)
 
