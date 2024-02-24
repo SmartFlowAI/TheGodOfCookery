310bf591526fdebc15728e834b56a4fdafa916ed
zhanghui_china <63148804+zhanghui-china@users.noreply.github.com>
2024-02-24 17:09:58 +0800

    Update download.py

--- download.py
+++ download.py
@@ -5,9 +5,10 @@ import whisper
 
 # download  shishen model
 model_dir = snapshot_download('zhanghuiATchina/zhangxiaobai_shishen2_full')
+
 os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
 
-# # download m3e model
+# download m3e model
 command_str = 'huggingface-cli download --resume-download moka-ai/m3e-base --local-dir-use-symlinks False --local-dir '+ os.environ.get('HOME') + '/models/m3e-base'
 os.system(command_str)
 
@@ -16,6 +17,6 @@ scales = ["tiny", "base", "small", "medium", "large"]
 for scale in scales:
     whisper.load_model(scale)
 
-# download taiyi model
+# download SD model
 command_str = 'huggingface-cli download --resume-download IDEA-CCNL/Taiyi-Stable-Diffusion-1B-Chinese-v0.1 --local-dir-use-symlinks False --local-dir '+ os.environ.get('HOME') + '/models/Taiyi-Stable-Diffusion-1B-Chinese-v0.1'
 os.system(command_str)
