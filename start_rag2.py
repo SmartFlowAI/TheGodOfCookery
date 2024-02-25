d6354c60d4bc8ab172b9df71ca47d80805a1e090
zhanghui_china <63148804+zhanghui-china@users.noreply.github.com>
2024-02-25 16:54:08 +0800

    Update start_rag2.py

--- start_rag2.py
+++ start_rag2.py
@@ -1,3 +1,4 @@
 import os
-#os.system('python download.py')
+os.system('python download.py')
+os.system('python download_rag2_model.py')
 os.system('streamlit run app-enhanced-rag.py --server.address=0.0.0.0 --server.port 7860')
