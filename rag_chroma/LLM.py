304894ed1d1d9545ebf96231be8164600700a439
SchweitzerGAO <1030587461@qq.com>
2024-02-25 10:29:48 +0800

    [RAG] An attempt to fix unrecognized eos in RAG with chroma

--- rag_chroma/LLM.py
+++ rag_chroma/LLM.py
@@ -10,13 +10,15 @@
 @Desc    :   基于InternLM模型自定义 LLM 类
 """
 
+from dataclasses import asdict
 from langchain.llms.base import LLM
 from typing import Any, List, Optional
 from langchain.callbacks.manager import CallbackManagerForLLMRun
 from transformers import AutoTokenizer, AutoModelForCausalLM
 # from modelscope import AutoModelForCausalLM, AutoTokenizer
 import torch
-
+from rag_chroma.interface import GenerationConfig, generate_interactive
+from app import combine_history
 
 class CookMasterLLM(LLM):
     # 基于本地 InternLM 自定义 LLM 类
@@ -39,8 +41,20 @@ class CookMasterLLM(LLM):
         # 重写调用函数
         system_prompt = "您是一个厨师，熟悉很多菜的制作方法。用户会问你哪些菜怎么制作，您可以用自己的专业知识答复他。回答的内容一般包含两块：这道菜需要哪些食材，这道菜具体是怎么做出来的。如果用户没有问菜谱相关的问题，就提醒他对菜谱的相关问题进行提问。"
         messages = [(system_prompt, '')]
-        response, history = self.model.chat(self.tokenizer, prompt, history=messages)
-        return response
+        # response, history = self.model.chat(self.tokenizer, prompt, history=messages)
+        # return response
+        config = GenerationConfig(
+        max_length=2048, top_p=0.8, temperature=0.8, repetition_penalty=1.002)   #InternLM2
+        generator = generate_interactive(
+                    model=self.model,
+                    tokenizer=self.tokenizer,
+                    prompt=combine_history(prompt),
+                    # additional_eos_token_id=103028,
+                    additional_eos_token_id=92542,
+                    **asdict(config),
+                )
+        for cur_response in generator:
+            yield cur_response
 
     @property
     def _llm_type(self) -> str:
