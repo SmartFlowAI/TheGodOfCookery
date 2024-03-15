8a42b34b2aba61d1c7c648b445018845203216e5
乐正萌 <804570270@qq.com>
2024-03-16 00:53:34 +0800

    加入了对包括模型在内的问答链的评测

--- eval/evaluate.py
+++ eval/evaluate.py
@@ -1,20 +1,25 @@
 from collections import Counter
 import re
 import json
+
+import torch
 from tqdm import tqdm
+
 import pickle
-from transformers.utils import logging
 from langchain.embeddings.huggingface import HuggingFaceEmbeddings
 from BCEmbedding.tools.langchain import BCERerank
+from langchain.prompts import PromptTemplate
 from langchain_community.vectorstores import FAISS
 from langchain_community.vectorstores import Chroma
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.retrievers import ContextualCompressionRetriever, EnsembleRetriever
-# from config import load_config
-from config_test.config_test import load_config
+from langchain.chains import RetrievalQA
+from config import load_config
+# from config_test.config_test import load_config
 from rag.HyQEContextualCompressionRetriever import HyQEContextualCompressionRetriever
-
-logger = logging.get_logger(__name__)
+from rag.interface import GenerationConfig
+from rag.CookMasterLLM import CookMasterLLM
+from modelscope import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 
 
 def load_vector_db():
@@ -61,6 +66,67 @@ def load_retriever():
     return compression_retriever
 
 
+def load_chain(llm, verbose=False):
+    # 加载检索器
+    retriever = load_retriever()
+
+    # RAG对话模板
+    qa_template = """先对上下文进行内容总结,再使上下文来回答用户的问题。总是使用中文回答。
+可参考的上下文：
+···
+{context}
+···
+问题: {question}
+如果给定的上下文无法让你做出回答，请根据你自己所掌握的知识进行回答。
+有用的回答:"""
+    QA_CHAIN_PROMPT = PromptTemplate(input_variables=["context", "question"],
+                                     template=qa_template)
+
+    # 单轮对话RAG问答链
+    qa_chain = RetrievalQA.from_chain_type(llm=llm,
+                                           chain_type="stuff",
+                                           chain_type_kwargs={"prompt": QA_CHAIN_PROMPT, "verbose": verbose},
+                                           retriever=retriever)
+
+    return qa_chain
+
+
+def load_model():
+    llm_model_path = load_config('llm', 'llm_model_path')
+    base_model_type = load_config('llm', 'base_model_type')
+    print(f"base model type:{base_model_type}")
+    max_length = 32768
+    if base_model_type == 'internlm-chat-7b':
+        generation_config = GenerationConfig(
+            max_length=max_length)  # InternLM1
+    elif base_model_type == 'internlm2-chat-1.8b':
+        generation_config = GenerationConfig(
+            max_length=max_length, top_p=0.8, temperature=0.8, repetition_penalty=1.17)  # InternLM2 1.8b need 惩罚参数
+    else:
+        generation_config = GenerationConfig(
+            max_length=max_length, top_p=0.8, temperature=0.8, repetition_penalty=1.002)  # InternLM2 2 need 惩罚参数
+    # int4 量化加载
+    quantization_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_compute_dtype=torch.float16,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_use_double_quant=True,
+    )
+    print("正在从本地加载模型...")
+    model = AutoModelForCausalLM.from_pretrained(llm_model_path, trust_remote_code=True, torch_dtype=torch.float16,
+                                                 device_map="auto",
+                                                 quantization_config=quantization_config).eval()
+    tokenizer = AutoTokenizer.from_pretrained(llm_model_path, trust_remote_code=True)
+    llm = CookMasterLLM(model, tokenizer)
+    model.generation_config.max_length = generation_config.max_length
+    model.generation_config.top_p = generation_config.top_p
+    model.generation_config.temperature = generation_config.temperature
+    model.generation_config.repetition_penalty = generation_config.repetition_penalty
+    print(model.generation_config)
+    print("完成本地模型的加载")
+    return model, tokenizer, llm
+
+
 def de_punct(output: str):
     rule = re.compile(r"[^a-zA-Z0-9\u4e00-\u9fa5]")
     output = rule.sub('', output)
@@ -86,21 +152,44 @@ def f1_score(output, gt):
     return f1
 
 
-def test_retriever(retriever, query) -> str:
-    docs = retriever.get_relevant_documents(query)
-    if len(docs) == 0:
-        return ""
-    return docs[0].page_content
-
-
-def evaluate(retriever):
+def evaluate_retriever():
     # 正式评估请手动替换下一行的路径
     with open('./eval_dataset_test.json', 'r', encoding='utf-8') as f:
         data = json.load(f)
     f1_sum = 0
+    retriever = load_retriever()
     for d in tqdm(data):
-        output = test_retriever(retriever, d["conversation"][0]['input'])
+        query = d["conversation"][0]['input']
+        docs = retriever.get_relevant_documents(query)
+        if len(docs) == 0:
+            output = ""
+        else:
+            output = docs[0].page_content
         gt = d["conversation"][0]['input'] + '\n' + d["conversation"][0]['output']
+        # print("--------------------输入：", query)
+        # print("--------------------输出：", output)
+        # print("--------------------答案：", gt)
+        f1_sum += f1_score(output, gt)
+    print(f'F1 score sum: {f1_sum}')
+    print(f'The number of data: {len(data)}')
+    print(f'F1 average: {f1_sum / len(data)}')
+
+
+def evaluate_model():
+    # 正式评估请手动替换下一行的路径
+    with open('./eval_dataset_test.json', 'r', encoding='utf-8') as f:
+        # 评测模型耗时较长，可以只评测部分数据
+        data = json.load(f)[:10]
+    f1_sum = 0
+    model, tokenizer, llm = load_model()
+    qa_chain = load_chain(llm)
+    for d in tqdm(data):
+        query = d["conversation"][0]['input']
+        output = qa_chain({"query": query})['result']
+        gt = d["conversation"][0]['input'] + '\n' + d["conversation"][0]['output']
+        print("--------------------输入：", query)
+        print("--------------------输出：", output)
+        print("--------------------答案：", gt)
         f1_sum += f1_score(output, gt)
     print(f'F1 score sum: {f1_sum}')
     print(f'The number of data: {len(data)}')
@@ -108,5 +197,5 @@ def evaluate(retriever):
 
 
 if __name__ == '__main__':
-    retriever = load_retriever()
-    evaluate(retriever)
+    evaluate_retriever()
+    # evaluate_model()
